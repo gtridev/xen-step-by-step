a2d2ceb2501f1b6aad8839f0c426ef2a4f7411c1
Milans Thapa <milan0.thapa1@gmail.com>
2013-06-17 13:48:18 +0545

    some cleanings...:D

--- main.sh
+++ main.sh
@@ -1,43 +1,83 @@
 #!/bin/bash
 
-@echo "Welcome to this xen step-by-step script...\
+WD="$(dirname $0)"
+PRG="$(basename $0)"
+
+function Usage {
+    echo -e "Usage: \tmy_gen [OPTIONS]"
+    echo -e "\t-i   |--install xen-hypervisor"
+    echo -e "\t-csr | --creat Storrage repository"
+    echo -e "\t-h   | --help\tDisplay this message"
+    exit
+}
+
+#check the number of arguments
+if [ $# -eq 0 ]; then
+    Usage;
+    exit 1;
+fi
+
+function init {
+#cat logo
+    cat ./logo
+    echo "Welcome to this xen step-by-step script...\
 	Its for Ubuntu Users only...sorry:(\
 	Plez go through https://help.ubuntu.xom/community/Xen "
-#cat logo
-cat logo
 
-#first get some updates on your system
-sudo apt-get update
+    
+    #first get some updates on your system
+    sudo apt-get update
+    
+    #install ssh server first...
+    #It isNot necessary that ssh server is pre-installed
+    sudo apt-get install openssh-server
+    
+}
 
-#install ssh server first...
-#It isNot necessary that ssh server is pre-installed
-sudo apt-get install openssh-server
+#init
+function xen_install {
+    
+    #init
+    #first Install xen 
+    #$sudo apt-get install xen-hypervisor
+    sudo apt-get install xen-hypervisor-amd64
+    
+    #adjust xen as a default boot opiton in grub menu and update grub
+    sed -i 's/GRUB_DEFAULT=.*\+/GRUB_DEFAULT="Xen 4.1-amd64"/' /etc/default/grub
 
-#first Install xen 
-#sudo apt-get install xen-hypervisor
-sudo apt-get install xen-hypervisor-amd64
+    #sudo update-grub
 
-#adjust xen as a default boot opiton in grub menu and update grub
-sudo sed -i 's/GRUB_DEFAULT=.*\+/GRUB_DEFAULT="Xen 4.1-amd64"/' /etc/default/grub
-#sudo update-grub
+    #Disable apparmor at boot 
+    sudo sed -i 's/GRUB_CMDLINE_LINUX=.*\+/GRUB_CMDLINE_LINUX="apparmor=0"/' /etc/default/grub
 
-#Disable apparmor at boot 
-sudo sed -i 's/GRUB_CMDLINE_LINUX=.*\+/GRUB_CMDLINE_LINUX="apparmor=0"/' /etc/default/grub
+    #Restrict "dom0" to 1GB of memory and 1 VCPU
+    sudo sed '/GRUB_CMDLINE_LINUX=/a\GRUB_CMDLINE_XEN="dom0_mem=1G,max:1G dom0_max_vcpus=1"' /etc/default/grub 
 
-#Restrict "dom0" to 1GB of memory and 1 VCPU
-sed '/GRUB_CMDLINE_LINUX=/a\GRUB_CMDLINE_XEN="dom0_mem=1G,max:1G dom0_max_vcpus=1"' /etc/default/grub 
+    #Update Grub with the config changes we just made
+    sudo update-grub
+    exit
 
-#Update Grub with the config changes we just made
-sudo update-grub
+    #Reboot the server so that Xen boots on the server
+    #sudo reboot
+}
 
-#Reboot the server so that Xen boots on the server
-sudo reboot
-
-#Once the server is back online ensure that Xen is running
-#cat /proc/xen/capabilities should display "control_d"
+function check_if_xen_installed {
+    #Once the server is back online ensure that Xen is running
+    cat /proc/xen/capabilities should display "control_d"
+}
 
 
-
+while true; do
+    case $1 in
+	-h|--help) Usage; exit;;
+	-i|--install) xen_install; break;;
+	-c|--chk) check_if_xen_installed;break;;
+	-x|--install_xapi) install_xapi; break;;
+	-n|--init) init; break;;
+	
+	--) break;;
+    esac 
+done
 
 
 
